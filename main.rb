8192df7f66150378e8edc4260e5ae32f1f3d26c4
Doppon <zaxpkuraudo7777@gmail.com>
2019-12-24 01:13:33 +0900

    コードを短縮

--- main.rb
+++ main.rb
@@ -1,14 +1,11 @@
 require 'open-uri'
 
-print("URL: ")
-url = gets.chomp
+print("URL: "); url = gets.chomp
 
 a = []
 
-open(url) {|file|
-  file.each_line do |line|
-    a << line
-  end
+open(url) { |file|
+  file.each_line { |line| a << line }
 }
 
 File.open("scan.html", "w") do |f|
