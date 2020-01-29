6c5c415ebb7a7a9329aa5f0bbdfda89a8b139568
Doppon <zaxpkuraudo7777@gmail.com>
2020-01-29 14:00:11 +0900

    必要な改行を追加

--- main.rb
+++ main.rb
@@ -2,17 +2,19 @@ require 'open-uri'
 
 print("URL: "); url = gets.chomp
 
+
 file_list = []
 url_length = url.length
 html_file_name = "scan.html"
 
+
 open(url) { |file|
   file.each_line { |line| file_list << line }
 }
-
 File.open(html_file_name, "w") do |f|
   f.puts(file_list.join)
 end
+
 
 f = File.open(html_file_name, "r")
 buffer = f.read
