77f0478ac26ccc2f7886876b38e12023217b2026
Doppon <zaxpkuraudo7777@gmail.com>
2020-01-29 13:44:52 +0900

    ファイルの書き込み変更の処理を加えてcssの読み込みを追加

--- main.rb
+++ main.rb
@@ -3,6 +3,7 @@ require 'open-uri'
 print("URL: "); url = gets.chomp
 
 a = []
+url_length = url.length
 
 open(url) { |file|
   file.each_line { |line| a << line }
@@ -11,3 +12,11 @@ open(url) { |file|
 File.open("scan.html", "w") do |f|
   f.puts(a.join)
 end
+
+f = File.open("scan.html","r")
+buffer = f.read();
+
+buffer.gsub!(/href=\"/, 'href="'+url[0..url_length-5])
+f = File.open("scan.html","w")
+f.write(buffer)
+f.close()
