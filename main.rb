139897c927ff6767f303b19305945c15b38715cc
Doppon <zaxpkuraudo7777@gmail.com>
2020-01-29 14:01:53 +0900

    コメントを追加

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require 'open-uri'
 
+# 入力受付
 print("URL: "); url = gets.chomp
 
 
@@ -8,6 +9,7 @@ url_length = url.length
 html_file_name = "scan.html"
 
 
+# HTTPリクエストによって取得したbodyをローカルファイルに書き込み
 open(url) { |file|
   file.each_line { |line| file_list << line }
 }
@@ -16,6 +18,7 @@ File.open(html_file_name, "w") do |f|
 end
 
 
+# CSSのパスを置換
 f = File.open(html_file_name, "r")
 buffer = f.read
 buffer.gsub!(/href=\"/, 'href="'+url[0..url_length-5])
