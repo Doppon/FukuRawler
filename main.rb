9d3e0c0a0d1257f99b9439e1e9a59f0b36012991
Doppon <zaxpkuraudo7777@gmail.com>
2020-01-29 13:56:30 +0900

    ファイル名をベタ書きしていたものを変数におきかえ

--- main.rb
+++ main.rb
@@ -4,18 +4,19 @@ print("URL: "); url = gets.chomp
 
 file_list = []
 url_length = url.length
+html_file_name = "scan.html"
 
 open(url) { |file|
   file.each_line { |line| file_list << line }
 }
 
-File.open("scan.html", "w") do |f|
+File.open(html_file_name, "w") do |f|
   f.puts(file_list.join)
 end
 
-f = File.open("scan.html","r")
+f = File.open(html_file_name,"r")
 buffer = f.read();
 buffer.gsub!(/href=\"/, 'href="'+url[0..url_length-5])
-f = File.open("scan.html","w")
+f = File.open(html_file_name,"w")
 f.write(buffer)
 f.close()
