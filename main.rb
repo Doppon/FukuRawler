d0e81de8843dd6b605634e0a378492076681a49a
Doppon <zaxpkuraudo7777@gmail.com>
2020-03-23 07:31:32 +0900

    階層的なディレクトリ作成を関数化

--- main.rb
+++ main.rb
@@ -38,6 +38,42 @@ def get_site_links(url)
   return hrefs + links
 end
 
+def loop_mkdir(mkdir_name, url, last_is_file)
+  # jp/shop/goto/bag
+  # ["jp", "shop", "goto", "bag"]
+  dir_names = mkdir_name.split("/")
+
+  if last_is_file
+    # ["jp", "shop", "goto", "bag", "hoge.css"]
+    # ["jp", "shop", "goto", "bag"]
+    dir_names = dir_names[0..-2]
+  end
+
+  # 配列の個数が 2 以上のとき、すなわち階層が 2 階層以上だった場合
+  if dir_names.length > 2
+    1.upto(dir_names.length) do |i|
+      dir_path = ""
+      max_count = i
+
+      i.times do |j|
+        dir_path += dir_names[j]
+        dir_path += "/" if j <= (max_count - 1)
+      end
+
+      # 例外処理をさせないとループから抜けてしまう
+      begin
+        Dir.mkdir(dir_path)
+        # puts("INFO: CREATED - #{dir_path}")
+      rescue => e
+        if e.message.length >= 11 && e.message[0..10] == "File exists"
+          next
+        end
+        puts(e)
+      end
+    end
+  end
+end
+
 # 入力受付
 print("URL: "); url = gets.chomp
 
@@ -68,48 +104,49 @@ hrefs.each do |l|
 
   # パス "#" の対策
   if l == "#"
-    puts("INFO: SKIP PATH #")
+    # puts("INFO: SKIP PATH #")
     next
+  end
+
+  # CSS 対策
+  # マッチング時にはマッチング箇所の数値が返り値
+  if (/.css/ =~ l)
+    # TODO: ディレクトリの作成
+    mkdir_name = ""
+    # 最初の / を切り取り | 基本的に "/" から始まってる
+    mkdir_name = l[1..-1] if l[0] == "/"
+    # 最後の / の切り取り( もし末尾に "/" が付いていた場合 )
+    mkdir_name = mkdir_name[0..-2] if mkdir_name[-1] == "/"
+    loop_mkdir(mkdir_name, l, true)
+
+
+    open(".#{l}", "wb") do |css|
+      # Ex. "/ac/globalfooter/5/ja_JP/styles/ac-globalfooter.built.css"
+      open(get_apple_domain(url) + l) do |io|
+        if io.content_type == "text/css"
+          css.write(io.read)
+          puts("INFO: CREATED - CSS - #{l}")
+        else
+          puts("ERROR: THE CONTENT TYPE IS #{io.content_type}.")
+        end
+      end
+    end
   end
 
   begin
     # jp配下のディレクトリ作成
     if l[0..3] == "/jp/"
-      # 最初の / を切り取り
-      mkdir_name = l[1..-1]
-
+      # ディレクトリの作成
+      mkdir_name = ""
+      # 最初の / を切り取り | 基本的に "/" から始まってる
+      mkdir_name = l[1..-1] if l[0] == "/"
+    
       # 最後の / の切り取り( もし末尾に "/" が付いていた場合 )
       mkdir_name = mkdir_name[0..-2] if mkdir_name[-1] == "/"
+      loop_mkdir(mkdir_name, l, false)
 
-      # jp/shop/goto/bag
-      # ["jp", "shop", "goto", "bag"]
-      dir_names = mkdir_name.split("/")
-      # 配列の個数が 2 以上のとき、すなわち階層が 2 階層以上だった場合
-      if dir_names.length > 2
-        1.upto(dir_names.length) do |i|
-          dir_path = ""
-          max_count = i
-
-          i.times do |j|
-            dir_path += dir_names[j]
-            dir_path += "/" if j <= (max_count - 1)
-          end
-
-          # 例外処理をさせないとループから抜けてしまう
-          begin
-            Dir.mkdir(dir_path)
-            puts("INFO: CREATED - #{dir_path}")
-          rescue => e
-            if e.message.length >= 11 && e.message[0..10] == "File exists"
-              next
-            end
-            puts(e)
-          end
-        end
-      end
-
-      Dir.mkdir(mkdir_name)
-      puts("INFO: CREATED - #{mkdir_name}")
+      Dir.mkdir(mkdir_name) unless mkdir_name.empty?
+      # puts("INFO: CREATED - #{mkdir_name}")
 
       # index作成
 
