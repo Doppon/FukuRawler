864644454904f4b60fd0f5591faa741bfc92a852
Doppon <zaxpkuraudo7777@gmail.com>
2020-04-06 18:05:50 +0900

    ファイルを並列処理によって読み込むサンプルプログラムの処理部分を追加

--- sample/concurrent.rb
+++ sample/concurrent.rb
@@ -1,3 +1,11 @@
 # ファイルを並列呼び出しするための関数
-def hoge(filename)
+def hoge(filenames)
+  h = {} # 戻り値の空ハッシュを作成
+
+  # スレッド
+  filenames.each do |filename|
+    h[filename] = Thread.new do
+      # ファイル読み込みの処理
+    end
+  end
 end
