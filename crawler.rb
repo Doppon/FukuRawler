06a906fe19bce68cce58b181a15f7f3eea98d96f
Doppon <zaxpkuraudo7777@gmail.com>
2020-04-09 22:31:53 +0900

    INFOを出力するように

--- crawler.rb
+++ crawler.rb
@@ -123,7 +123,7 @@ hrefs.each do |l|
 
   # パス "#" の対策
   if l == "#"
-    # puts("INFO: SKIP PATH #")
+    puts("INFO: SKIP PATH #")
     next
   end
 
@@ -142,7 +142,7 @@ hrefs.each do |l|
     else
       Dir.mkdir(mkdir_name)
     end
-    # puts("INFO: CREATED - #{mkdir_name}")
+    puts("INFO: CREATED - #{mkdir_name}")
 
     open(get_apple_domain(url) + "/" + mkdir_name) do |io|
       # index.html の作成
@@ -227,7 +227,7 @@ open(open_file_path, "r") do |f|
     else
       Dir.mkdir(mkdir_name)
     end
-    # puts("INFO: CREATED - #{mkdir_name}")
+    puts("INFO: CREATED - #{mkdir_name}")
 
 
     # リンク先の取得( 画像 )
