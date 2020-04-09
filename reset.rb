635cf5d58862b102113944f7d9419cd111d004d2
Doppon <zaxpkuraudo7777@gmail.com>
2020-04-09 22:43:09 +0900

    reset.rbを追加

---
+++ reset.rb
@@ -0,0 +1,14 @@
+system("rm -rf ac/")
+print("INFO: DELETED ac/")
+
+system("rm -rf jp/")
+print("INFO: DELETED jp/")
+
+system("rm -rf choose-country-region/")
+print("INFO: DELETED choose-country-region/")
+
+system("rm -rf metrics/")
+print("INFO: DELETED metrics/")
+
+system("rm -rf v/")
+print("INFO: DELETED v/")
