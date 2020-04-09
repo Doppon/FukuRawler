af6373a2f1d553e571555326c15dcc2214815f41
Doppon <zaxpkuraudo7777@gmail.com>
2020-04-09 23:10:35 +0900

    改行が含まれるように

--- reset.rb
+++ reset.rb
@@ -1,14 +1,14 @@
 system("rm -rf ac/")
-print("INFO: DELETED ac/")
+puts("INFO: DELETED ac/")
 
 system("rm -rf jp/")
-print("INFO: DELETED jp/")
+puts("INFO: DELETED jp/")
 
 system("rm -rf choose-country-region/")
-print("INFO: DELETED choose-country-region/")
+puts("INFO: DELETED choose-country-region/")
 
 system("rm -rf metrics/")
-print("INFO: DELETED metrics/")
+puts("INFO: DELETED metrics/")
 
 system("rm -rf v/")
-print("INFO: DELETED v/")
+puts("INFO: DELETED v/")
