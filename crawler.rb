5002222c662c88a50a33c6dbe9c484d730b63a4f
Doppon <zaxpkuraudo7777@gmail.com>
2020-04-12 20:15:41 +0900

    相対パスの場合だけ出なく絶対パスの場合の擬似コードも追加

--- crawler.rb
+++ crawler.rb
@@ -103,8 +103,16 @@ class Crawler
         #   File.dirname(css_file_path) + "/" + background_images.first[22..-2]
         #     "./ac/globalnav/5/ja_JP/styles/../images/globalnav/apple/image_large.svg"
         #
+        #
         # background_image が /v/home/d/~ などの場合
-        #   wip
+        #   css_file_path
+        #     "./ac/globalfooter/5/ja_JP/styles/ac-globalfooter.built.css"
+        #
+        #   background_images.last[22..-2]
+        #     "/ac/flags/1/images/jp/32.png"
+        #
+        #   ".#{background_images.last[22..-2]}"
+        #     "./ac/flags/1/images/jp/32.png"
 
         h[background_image] = Thread.new do
           background_image_url = background_image[22..-2] # background-image:url("---" の中身が取得できる
